fefdd826b3464cc7d9d60d616f0cd2182284c808
Victor Vilar <victor.svilar@gmail.com>
2022-10-24 19:51:53 -0300

    primeiro commit

---
+++ src/main/java/documentando/api/rest/DocumentandoApiRestApplication.java
@@ -0,0 +1,13 @@
+package documentando.api.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumentandoApiRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocumentandoApiRestApplication.class, args);
+	}
+
+}
