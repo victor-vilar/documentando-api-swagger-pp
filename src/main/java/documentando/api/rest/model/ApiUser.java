013a7957843c898c7f4c94944b4e6a9698a0583e
Victor Vilar <victor.svilar@gmail.com>
2022-10-25 19:44:12 -0300

    adicionado repositorio.

--- src/main/java/documentando/api/rest/model/ApiUser.java
+++ src/main/java/documentando/api/rest/model/ApiUser.java
@@ -15,6 +15,30 @@ public class ApiUser {
 		this.password = password;
 	}
 
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
 	@Override
 	public String toString() {
 		return "User{"+
--- src/main/java/documentando/api/rest/repository/ApiUserRepository.java
+++ src/main/java/documentando/api/rest/repository/ApiUserRepository.java
@@ -1,5 +1,41 @@
 package documentando.api.rest.repository;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import documentando.api.rest.model.ApiUser;
+
+/**
+ * Classe utilizada somente para simular um repository
+ * @author Victor
+ *
+ */
+
+@Repository
 public class ApiUserRepository {
 
+	public void save(ApiUser usuario) {
+		if(usuario.getId() == null) {
+			System.out.println("Usuario salvo");
+		}else {
+			System.out.println("Usuario Atualizado");
+		}
+	}
+	
+	public void deleteById(Integer id) {
+		System.out.print("Usuario de ID deletado");
+	}
+	
+	public List<ApiUser> findAll(){
+		List<ApiUser> usuarios = new ArrayList<>();
+		usuarios.add(new ApiUser("teste","senha"));
+		usuarios.add(new ApiUser("teste2","senha2"));
+		usuarios.add(new ApiUser("teste3","senha3"));
+		return usuarios;
+		
+	}
+	
+	
 }
